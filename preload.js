f5ca56f8e6a3cb09cc30bff05c78d67e7213a7ab
jakeWheaton <jake@codewithus.com>
2024-08-26 15:56:00 -0700

    Updating font locations

--- preload.js
+++ preload.js
@@ -1,7 +1,7 @@
 var scratchFont1, scratchFont2, scratchFont3
 
 function preload(){
-  scratchFont1 = loadFont("Fonts/Mouse_Memoirs/MouseMemoirs-Regular.ttf");
-  scratchFont2 = loadFont("Fonts/Indie_Flower/IndieFlower-Regular.ttf");
-  scratchFont3 = loadFont("Fonts/Mochiy_Pop_One/MochiyPopOne-Regular.ttf")
+  scratchFont1 = loadFont("https://github.com/jakeWheaton/Concept-Challenges/tree/main/Fonts/Mouse_Memoirs/MouseMemoirs-Regular.ttf");
+  scratchFont2 = loadFont("https://github.com/jakeWheaton/Concept-Challenges/tree/main/Fonts/Indie_Flower/IndieFlower-Regular.ttf");
+  scratchFont3 = loadFont("https://github.com/jakeWheaton/Concept-Challenges/tree/main/Fonts/Mochiy_Pop_One/MochiyPopOne-Regular.ttf")
 }
