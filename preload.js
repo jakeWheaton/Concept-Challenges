76f1d4af4c3f21d6a5f05aa96a078a22d23f008a
jakeWheaton <jake@codewithus.com>
2024-08-26 16:17:31 -0700

    Local Fonts
    Switched to built in fonts for local development

--- preload.js
+++ preload.js
@@ -1,7 +1,7 @@
 var scratchFont1, scratchFont2, scratchFont3
 
 function preload(){
-  scratchFont1 = loadFont("https://github.com/jakeWheaton/Concept-Challenges/tree/main/Fonts/Mouse_Memoirs/MouseMemoirs-Regular.ttf");
-  scratchFont2 = loadFont("https://github.com/jakeWheaton/Concept-Challenges/tree/main/Fonts/Indie_Flower/IndieFlower-Regular.ttf");
-  scratchFont3 = loadFont("https://github.com/jakeWheaton/Concept-Challenges/tree/main/Fonts/Mochiy_Pop_One/MochiyPopOne-Regular.ttf")
+  scratchFont1 = 'Courier New'//loadFont("https://github.com/jakeWheaton/Concept-Challenges/tree/main/Fonts/Mouse_Memoirs/MouseMemoirs-Regular.ttf");
+  scratchFont2 = 'Courier New'//loadFont("https://github.com/jakeWheaton/Concept-Challenges/tree/main/Fonts/Indie_Flower/IndieFlower-Regular.ttf");
+  scratchFont3 = 'Courier New'//loadFont("https://github.com/jakeWheaton/Concept-Challenges/tree/main/Fonts/Mochiy_Pop_One/MochiyPopOne-Regular.ttf")
 }
