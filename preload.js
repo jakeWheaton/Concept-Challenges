95f89055a4efa1b76687512c1e78aac403c90408
jakeWheaton <jake@codewithus.com>
2024-08-27 17:46:10 -0700

    Update preload.js

--- preload.js
+++ preload.js
@@ -2,9 +2,9 @@ var scratchFont1, scratchFont2, scratchFont3
 
 function preload(){
 	try {
-		scratchFont1 = loadFont("Fonts/Mouse_Memoirs/MouseMemoirs-Regular.ttf");
-		scratchFont2 = loadFont("Indie_Flower/IndieFlower-Regular.ttf");
-		scratchFont3 = loadFont("Fonts/Mochiy_Pop_One/MochiyPopOne-Regular.ttf")
+		scratchFont1 = loadFont("https://github.com/jakeWheaton/Concept-Challenges/tree/main/Fonts/Mouse_Memoirs/MouseMemoirs-Regular.ttf");
+		scratchFont2 = loadFont("https://github.com/jakeWheaton/Concept-Challenges/tree/main/Fonts/Indie_Flower/IndieFlower-Regular.ttf");
+		scratchFont3 = loadFont("https://github.com/jakeWheaton/Concept-Challenges/tree/main/Fonts/Mochiy_Pop_One/MochiyPopOne-Regular.ttf")
 	}
 	catch(err) {
 		scratchFont1 = 'Courier New';
