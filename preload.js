1d179d6d9ff62cec516b395116e90d5b5a72a147
jakeWheaton <jake@codewithus.com>
2024-08-27 17:41:36 -0700

    error handling for fonts

--- preload.js
+++ preload.js
@@ -1,7 +1,14 @@
 var scratchFont1, scratchFont2, scratchFont3
 
 function preload(){
-  scratchFont1 = 'Courier New'//loadFont("https://github.com/jakeWheaton/Concept-Challenges/tree/main/Fonts/Mouse_Memoirs/MouseMemoirs-Regular.ttf");
-  scratchFont2 = 'Courier New'//loadFont("https://github.com/jakeWheaton/Concept-Challenges/tree/main/Fonts/Indie_Flower/IndieFlower-Regular.ttf");
-  scratchFont3 = 'Courier New'//loadFont("https://github.com/jakeWheaton/Concept-Challenges/tree/main/Fonts/Mochiy_Pop_One/MochiyPopOne-Regular.ttf")
+	try {
+		scratchFont1 = loadFont("Fonts/Mouse_Memoirs/MouseMemoirs-Regular.ttf");
+		scratchFont2 = loadFont("Indie_Flower/IndieFlower-Regular.ttf");
+		scratchFont3 = loadFont("Fonts/Mochiy_Pop_One/MochiyPopOne-Regular.ttf")
+	}
+	catch(err) {
+		scratchFont1 = 'Courier New';
+		scratchFont2 = 'Courier New';
+		scratchFont3 = 'Courier New';
+	}
 }
